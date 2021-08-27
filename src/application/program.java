ef0bcabc3c3a30ca7ad7e6b32dfc622b3a921da0
Leonardo Age Netto <leonardoage@gmail.com>
2021-08-26 23:40:05 -0300

    Criação de classes iniciais

---
+++ src/application/program.java
@@ -0,0 +1,18 @@
+package application;
+
+import chess.ChessMatch;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ChessMatch game = new ChessMatch();
+		
+		UI.printBoard(game.getPieces());
+		
+		
+		
+	}
+
+}
