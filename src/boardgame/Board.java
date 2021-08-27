ae35d7f81afff6a40c979afb44962cc9bc47a897
Leonardo Age Netto <leonardoage@gmail.com>
2021-08-27 19:43:14 -0300

    Tratamento de exceção - BoardException
    Tratamento de exceção.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -2,13 +2,16 @@ package boardgame;
 
 public class Board {
 
-	private Integer rows; 
-	private Integer columns; 
-	private Piece[][] pieces; 
-	
-	public Board (Integer rows, Integer columns) { 
-		this.rows = rows; 
-		this.columns = columns; 
+	private Integer rows;
+	private Integer columns;
+	private Piece[][] pieces;
+
+	public Board(Integer rows, Integer columns) {
+		if (rows <= 0 || columns <= 0) {
+			throw new BoardException("Erro criando o tabuleiro: precisamos a medida mínima é de 1 linha e 1 coluna ");
+		}
+		this.rows = rows;
+		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
 
@@ -16,29 +19,53 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) { 
+			throw new BoardException("Position not on the board.");
+		}
+		return pieces[row][column];
 	}
-	
-	public Piece piece(int row, int column) { 
-		return pieces[row][column]; 
+
+	public Piece piece(Position position) {
+		if (!positionExists(position)) { 
+			throw new BoardException("Position not on the board.");
+		}
+		
+		return pieces[position.getRow()][position.getColumn()];
 	}
-	
-	public Piece piece(Position position) { 
-		return pieces[position.getRow()][position.getColumn()]; 
-	}
-	
-	public void placePiece(Piece piece, Position position) { 
+
+	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) { 
+			throw new BoardException("There is a piece on position " + position); 
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
-		piece.position = position;  
+		piece.position = position;
 	}
-	
+
+	public Piece removePiece(Position position) {
+		return null;
+	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
+	}
+
+	public Boolean positionExists(Position position) {
+		// Chamou a função privada para confirmar se a posição existe,
+		// pergunto-me se não seria o caso de escrever diretamente a validação
+		// aqui nesse método ao invés de chamar a funçãoprivada
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public Boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) { 
+			throw new BoardException("Position not on the board.");
+		}
+				
+		return piece(position) != null;
+	}
 }
