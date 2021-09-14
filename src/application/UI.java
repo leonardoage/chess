d616470752e5b5aae8ee0e49883e817274441ade
Leonardo Age Netto <leonardoage@gmail.com>
2021-09-13 22:01:08 -0300

    156. ChessException e ChessPosition

--- src/application/UI.java
+++ src/application/UI.java
@@ -7,6 +7,7 @@ import chess.ChessPiece;
 public class UI {
 	
 	public static void printBoard(ChessPiece [][] pieces) {
+		System.out.println("  a b c d e f g h  ");
 		for	(int i = 0;  i < pieces.length; i++) { 
 			System.out.print( pieces.length - i + " ");
 			for (int j = 0; j < pieces.length; j++) { 
@@ -14,7 +15,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h  ");
+
 		
 	}
 	
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,43 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	private char column;
+	private int row;
+
+	public ChessPosition(char column, int row) {
+
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException(
+					"Error instantiating ChessPosition. Valid values colum a - h and row between 1 and 8");
+		}
+		this.column = column;
+		this.row = row;
+
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	protected Position toPosition() { 
+		return new Position(8 - row, column - 'a'); 
+	}
+	
+	protected static ChessPosition fromPosition(Position p) {
+		return new ChessPosition( (char)('a'- p.getColumn()), 8 - p.getRow()); 
+	}
+	
+	@Override
+	public String toString() { 
+		return "" + column + row; // String vazio para forçar concatenação de string
+		
+	}
+	
+
+}
